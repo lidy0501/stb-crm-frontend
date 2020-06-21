3d5eca3458dc30509b82dbfec01859878d846323
lidy0501@163.com <lidy0501@163.com>
2020-06-22 01:55:17 +0800

    脚本

---
+++ start.sh
@@ -0,0 +1,23 @@
+# git diff 查看修改的内容
+git diff
+
+#撤回全部修改的内容
+git checkout .
+
+
+
+# 拉代码
+git pull
+
+#  安装依赖和打包
+
+npm install
+
+npm run build
+
+
+#启动容器
+#docker run -d --restart=on-failure:5\
+#    -p 9090:80 \
+#    -v $PWD/dist:/usr/share/nginx/html \
+#    --name stb-crm-frontend nginx
