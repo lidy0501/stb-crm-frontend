a0ff49a1edd3b332ea5a78cf8a312cad8e7db368
lidy@raiyee.com <lidy@raiyee.com>
2020-08-30 11:59:39 +0800

    sku新增

--- src/views/Manage/Goods/SKUSet.vue
+++ src/views/Manage/Goods/SKUSet.vue
@@ -8,8 +8,20 @@
           <div>SKU编码</div>
           <div>单位</div>
           <div>花色</div>
+          <div class="operate-box">
+            <i class="iconfont icon-jiahao" @click="addSku"></i>
+          </div>
         </div>
-        <div class="sku-item" v-for="(item, index) in skuList">
+        <div class="sku-item" v-for="(old, index) in oldSkuList">
+          <div>{{old.skuCode}}</div>
+          <div>{{old.skuUnit}}</div>
+          <div>{{old.skuColor}}</div>
+          <div class="operate-box">
+            <i class="iconfont icon-jianhao" @click="deleteSku('old', index)"></i>
+          </div>
+        </div>
+
+        <div class="sku-item" v-for="(item, index) in newSkuList">
           <div>
             <input type="text" placeholder="请输入编码" v-model="item.skuCode">
           </div>
@@ -20,17 +32,15 @@
             <input type="text" placeholder="请输入花色" v-model="item.skuColor">
           </div>
           <div class="operate-box">
-            <i v-if="skuList.length !== 1" class="iconfont icon-jianhao" @click="deleteSku(index)"></i>
-            <i v-if="index === skuList.length - 1" class="iconfont icon-jiahao" @click="addSku"></i>
+            <i class="iconfont icon-jianhao" @click="deleteSku('new', index)"></i>
           </div>
         </div>
       </div>
     </div>
     <div class="errorTop">{{errorTip}}</div>
     <div class="footer-btn">
-      <footer-btn @goBack="goBack" @save="save"></footer-btn>
+      <footer-btn @goBack="goBack" @save="save" v-if="newSkuList.length > 0"></footer-btn>
     </div>
-    <!--<ErrorTipsBox></ErrorTipsBox>-->
 
   </div>
 </template>
@@ -40,63 +50,90 @@
   import TopHead from '../../../components/TopHead/TopHead.vue'
   import {OPEN_TIP_OPERATE_BOX, OPEN_TOAST, OPEN_ERROR_TIP_BOX} from '../../../store/constants/home'
   import FooterBtn from '../../../components/FooterBtn/FooterBtn.vue'
-  import ErrorTipsBox from '../../../components/ErrorTipsBox/ErrorTipsBox.vue'
 
 
 
   export default {
     name: 'SKUSet',
-    components: {TopHead, FooterBtn, ErrorTipsBox},
+    components: {TopHead, FooterBtn},
     data() {
       return {
-        errorTips: [],
-        skuList: [{
-          skuId: '1', skuCode: '1', skuUnit: '台', skuColor: '黄色'
-          },
-          {
-
-          },
-          {
-          }, {}],
+        oldSkuList: [], // 已经存在的skuList
+        newSkuList: [],
         errorTip: '',
-        inputFill: true
+        inputFill: true,
+        repeat: false // true表有重复的sku编码
       }
     },
+    mounted() {
+      this.init()
+    },
     methods: {
-      deleteSku(index) {
-        console.log(1)
-        this.$store.commit(OPEN_ERROR_TIP_BOX, '不能删除')
-        console.log(2)
-        return
-        if (!this.skuList[index].skuId) { // 说明是新加的
-          this.skuList.splice(index, 1);
+      init() {
+        this.$http.post('/GoodsController/querySkuList').then(res => {
+          const data = res.data
+          this.oldSkuList = data
+        })
+      },
+      deleteSku(type, index) {
+        if (type === 'new') { // 说明是新加的, 直接删除
+          this.newSkuList.splice(index, 1);
         } else { // 已经存在的sku，需要检查这个sku有没有在使用，如果已经在使用，是不能删除的
         }
       },
       addSku() {
-        this.skuList.push({});
+        this.newSkuList.push({skuId: '', skuCode: '', skuUnit: '', skuColor: ''});
       },
       goBack() {
         this.$route.go(-1)
       },
       save() {
-        this.$store.commit(OPEN_ERROR_TIP_BOX, '不能删除')
-        return
+        //this.$store.commit(OPEN_ERROR_TIP_BOX, '不能删除')
         this.errorTip = ''
         this.inputFill = true
+        this.repeat = false
         this.verifySkuInput()
         if (!this.inputFill) {
           return
         }
-        this.$store.commit(OPEN_TOAST, '保存成功')
+        this.verifyRepeat()
+        if (this.repeat) {
+          return
+        }
+        this.$http.post('/GoodsController/addSku', this.newSkuList).then(res => {
+          const data = res.data
+          if (data.code === 0) {
+            this.$store.commit(OPEN_TOAST, '保存成功')
+            this.newSkuList = []
+            this.init()
+          } else {
+            this.$store.commit(OPEN_ERROR_TIP_BOX, data.message)
+          }
+        })
       },
       verifySkuInput() {
-        this.skuList.forEach(sku => {
+        // 校验输入框
+        this.newSkuList.forEach(sku => {
           if (!sku.skuCode || !sku.skuUnit || !sku.skuColor) {
             this.errorTip = '请输入完整的sku信息';
             this.inputFill = false;
           }
-        });
+        })
+      },
+      verifyRepeat () {
+        let list = []
+        list.push(...this.oldSkuList)
+        list.push(...this.newSkuList)
+        console.log(list)
+        for (let i = 0; i < list.length - 1; i++) {
+          for (let j = i + 1; j <= list.length - 1; j++) {
+            if (list[i].skuCode === list[j].skuCode) {
+              this.repeat = true
+              this.errorTip = 'SKU编码：' + list[i].skuCode + '重复出现，请修改后保存！'
+              break
+            }
+          }
+        }
       }
     }
   }
